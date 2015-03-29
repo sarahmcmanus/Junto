2e8b2dd4a65f1e066d7971b6cd75063ccb725e08
Matt Parsons <parsonsmatt@gmail.com>
2015-03-28 22:22:37 -0400

    Cleanup Gemfile, Update Ruby version in bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,10 +2,9 @@
 
 # Install rvm and ruby:
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-2.2.0 --with-default-gems="bundler"
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-2.2.1 --with-default-gems="bundler"
 echo 'gem: --no-document' >> ~/.gemrc
 source /home/vagrant/.rvm/scripts/rvm
-gem install bundler
 
 # Set the application up
 cd /vagrant
