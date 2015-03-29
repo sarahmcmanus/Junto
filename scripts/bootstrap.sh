5089d2df039d818e29e00745c8b9f690b6e63ff4
Matt Parsons <parsonsmatt@gmail.com>
2015-03-28 22:52:26 -0400

    createrole for postgresql

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/bash 
 
 # Install rvm and ruby:
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
--- scripts/rootstrap.sh
+++ scripts/rootstrap.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 apt-get update
-apt-get install git node postgresql-9.3 postgresql-contrib-9.3 curl -y
+apt-get upgrade -y
+apt-get install git node postgresql-9.3 libpq-dev postgresql-contrib-9.3 curl -y
+
+# PostgreSQL setup
+sudo -u postgres createuser -s vagrant 
 
