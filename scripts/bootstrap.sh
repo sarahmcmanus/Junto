58530949cbea210227a157ad3c10056992ce8c8f
Matt Parsons <parsonsmatt@gmail.com>
2015-03-28 23:07:38 -0400

    fix script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,10 +4,11 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby=ruby-2.2.1 --with-default-gems="bundler"
 echo 'gem: --no-document' >> ~/.gemrc
-source /home/vagrant/.rvm/scripts/rvm
+source ~/.rvm/scripts/rvm
 
 # Set the application up
 cd /vagrant
+
 bundle install
 rake db:setup
 gem install foreman
